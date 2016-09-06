8347a043a68c4e84cb254777c1b83e8729d909e7
Yevhenii Bohdanets <yevhenii.bohdanets@gmail.com>
2016-09-06 13:25:53 +0300

    Add fab animation to MachineSettingsActivity

--- app/src/main/java/io/github/evilofsoul/openwol/MachineSettingsActivity.java
+++ app/src/main/java/io/github/evilofsoul/openwol/MachineSettingsActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 
 import io.github.evilofsoul.openwol.core.MacAddress;
@@ -63,6 +65,8 @@ public class MachineSettingsActivity extends AppCompatActivity implements View.O
 
         final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab2);
         fab.setOnClickListener(this);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.simple_grow);
+        fab.setAnimation(animation);
     }
 
     @Override
